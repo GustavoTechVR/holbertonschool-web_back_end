9df748d5838935a7049f4c97e774903d0c900e46
Gustavo Jomeque Maruyama <32277272+GustavoTechVR@users.noreply.github.com>
2024-02-01 05:23:31 -0500

    Feat: added 0-add.py README.md

---
+++ 0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module with a type-annotated function add
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Function that takes two floats and returns their sum.
+
+    Args:
+        a (float): The first input float.
+        b (float): The second input float.
+
+    Returns:
+        float: The sum of the input floats.
+    """
+    return a + b
